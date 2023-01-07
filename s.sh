494fd080cfff7ff6305e2624e030a6b90f34a0d9
BlackGhostUK1 <85413150+BlackGhostUK1@users.noreply.github.com>
2023-01-07 20:11:44 +0330

    Delete s.sh

--- s.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Enter your Embedded links: " && cat > t.txt
-grep -Po "https://[^']+" t.txt | cut -d"\"" -f1 | sed "s/embed/download/" >> t.txt 
-sed -i '/</d' t.txt
-echo "Subject:Your download links comin n hot!" | msmtp gameali774@hotmail.com < t.txt
